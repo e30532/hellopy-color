e2dc0a5ba24d1a8f8c9af303f1b55366b22f101c
yamayoshi <yamayoshi@192.168.3.10>
2019-08-06 19:09:16 +0900

    add a read file function

--- app.py
+++ app.py
@@ -26,6 +26,12 @@ def main():
 @app.route("/color/<new_color>")
 def new_color(new_color):
         return render_template('hello.html', name=socket.gethostname(), color=color_codes[new_color])
+
+@app.route("/read_file")
+def read_file():
+    f = open("/data/testfile.txt")
+    contents = f.read()
+    return render_template('hello.html', name=socket.gethostname(), contents=contents, color=color_codes[new_color])
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port="8080") 
